08027b4b365e84ff9c0229c514f29bfb31fb01b4
Adrian Schauer <adrian.schauer@aon.at>
2024-04-14 22:57:05 +0200

    RabbitMQ.
    Co-authored-by: Fabian Lasser <las20298@spengergasse.at>
    Co-authored-by: Lindor Limani <lindor.limani@gmail.com>

---
+++ Spg.Payment/src/Spg.Payment.Application/Services/RabbitService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+
+namespace Spg.Payment.Application.Services
+{
+    public class RabbitService
+    {
+        private readonly RabbitMQ.Client.IModel _channel;
+
+        public RabbitService(RabbitMQ.Client.IModel channel)
+        {
+            _channel = channel;
+        }
+
+        public void SendMessage(string queue, string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+            _channel.BasicPublish(exchange: "", routingKey: queue, basicProperties: null, body: body);
+        }
+
+        public string ReceiveMessage(string queue)
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+            string message = null;
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                message = Encoding.UTF8.GetString(body);
+                _channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+
+            _channel.BasicConsume(queue: queue, autoAck: false, consumer: consumer);
+            return message;
+        }
+    }
+}
--- Spg.Payment/src/Spg.Payment.Payment/Controllers/CheckMarkController.cs
+++ Spg.Payment/src/Spg.Payment.Payment/Controllers/CheckMarkController.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using MediatR;
 using Spg.Payment.DomainModel.Interfaces;
+using Spg.Payment.Application.Services;
+using MassTransit;
 
 namespace Spg.Payment.Payment.Controllers
 {
@@ -17,20 +19,29 @@ namespace Spg.Payment.Payment.Controllers
         private readonly IConfiguration _configuration;
         private readonly ILogger<CheckMarkController> _logger;
         private readonly IPaymentService _paymentService;
+        //private readonly IPublishEndpoint _publishEndpoint;
 
-        public CheckMarkController(IWebHostEnvironment env, IConfiguration configuration, ILogger<CheckMarkController> logger, IPaymentService paymentService)
+        public CheckMarkController(IWebHostEnvironment env, IConfiguration configuration, ILogger<CheckMarkController> logger, IPaymentService paymentService)//, IPublishEndpoint publishEndpoint)
         {
             //_mediator = mediator;
             _env = env;
             _configuration = configuration;
             _logger = logger;
             _paymentService = paymentService;
+            //_publishEndpoint = publishEndpoint;
         }
 
         [HttpGet]
         public async Task<IActionResult> IsVerifiedTill([FromQuery] int userId)
         {
             var isVerifiedTill = await _paymentService.IsVerifiedTill(userId);
+
+            //_publishEndpoint.Publish(new
+            //{
+            //    UserId = userId,
+            //    VerifiedTill = isVerifiedTill
+            //});
+
             return Ok(isVerifiedTill);
         }
     }
